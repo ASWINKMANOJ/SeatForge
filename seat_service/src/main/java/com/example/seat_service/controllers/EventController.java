e759550330d274c38eb2057c4cd4c04d2973f1b4
aswinkm <aswinkmanoj101@gmail.com>
2026-02-12 22:34:33 +0530

    setup

---
+++ seat_service/src/main/java/com/example/seat_service/controllers/EventController.java
@@ -0,0 +1,23 @@
+package com.example.seat_service.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getEvents() {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+
+        return ResponseEntity.ok(response);
+    }
+}
