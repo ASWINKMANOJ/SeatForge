82c8fc87dad0f7b01abd19334ec3739ff0111c8b
ASWINKMANOJ <aswinkmanoj101@gmail.com>
2026-03-24 07:57:30 +0530

    clean up

--- seat_service/src/main/java/com/example/seat_service/config/security/Auth0JwtConverter.java
+++ seat_service/src/main/java/com/example/seat_service/config/security/Auth0JwtConverter.java
@@ -1,6 +1,5 @@
 package com.example.seat_service.config.security;
 
-
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
